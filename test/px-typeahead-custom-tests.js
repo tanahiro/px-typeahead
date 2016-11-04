9701adbd4df546791a1bc4d6130b83c816b6163c
Vermel, Runn (GE Global Research) <runn.vermel@ge.com>
2016-11-04 15:29:48 -0700

    added gulp, removed grunt, re-wrote many pieces of the code, cleaned up sass, fixed broken test

--- test/px-typeahead-custom-tests.js
+++ test/px-typeahead-custom-tests.js
@@ -19,16 +19,16 @@ function runCustomTests() {
   suite('Custom Automation Tests for px-typeahead', function() {
 
     //this.timeout(30000);
-   
+
     test('Check initial initial state of px-typeahead', function(done){
       var El = Polymer.dom(document).querySelector('px-typeahead'),
           ValueEl = Polymer.dom(El.root).querySelector('input');
       assert.equal(ValueEl.textContent, '');
       done();
     });
-    
 
-    test('Making sure of px-typehead input will show outline when focussed', function(done){
+
+    test('Making sure px-typehead input will show outline when focussed', function(done){
       var typeaheadEl = Polymer.dom(this.root).querySelector('px-typeahead'),
           typeaheadInputEl = Polymer.dom(typeaheadEl.root).querySelector('input');
           assert.equal(typeaheadInputEl.textContent, '');
@@ -36,14 +36,12 @@ function runCustomTests() {
           typeaheadInputEl.focus();
           fireFocusEvent(typeaheadInputEl);
           typeaheadInputEl.textContent = 'a';
-          var node = Polymer.dom(this.root).node.querySelectorAll('.parentdiv');
-          var classList = node[0].classList;
-          assert.equal(classList[5],'focus');
+          assert.isTrue(typeaheadEl.$$('#mainContainer').classList.contains('focus'));
           done();
-          
+
     });
 
-    test('Making sure of px-typehead input type', function(done){
+    test('Making sure px-typehead input type', function(done){
       var typeaheadEl = Polymer.dom(this.root).querySelector('px-typeahead'),
           typeaheadInputEl = Polymer.dom(typeaheadEl.root).querySelector('input');
           assert.equal(typeaheadInputEl.textContent, 'a');
@@ -53,14 +51,14 @@ function runCustomTests() {
           assert.equal(typeaheadInputEl.textContent,'a');
           assert.equal(typeaheadInputEl.value,'');
           done();
-          
+
     });
 
     test('Making sure that px-typeahead shows suggestions', function(done){
-      
+
           var onKeyupHandle = function(e) {
             var container = Polymer.dom(this.root).querySelector('.container');
-            assert.isTrue(!container);  
+            assert.isTrue(!container);
           };
           var typeaheadEl = Polymer.dom(document).querySelector('px-typeahead'),
               typeaheadInputEl = Polymer.dom(typeaheadEl.root).querySelector('input');
@@ -72,16 +70,16 @@ function runCustomTests() {
           done();
           flush(() => {
               expect(document.getElementsByClassName('container').length).to.equal(1);
-              
-    
+
+
           });
       });
 
       test('Making sure based on input provided - typeahead shows 3 suggestions', function(done){
-      
+
           var onKeyupHandle = function(e) {
             var container = Polymer.dom(this.root).querySelector('.container');
-            assert.isTrue(!container);  
+            assert.isTrue(!container);
           };
           var typeaheadEl = Polymer.dom(document).querySelector('px-typeahead'),
               typeaheadInputEl = Polymer.dom(typeaheadEl.root).querySelector('input');
@@ -93,7 +91,7 @@ function runCustomTests() {
           done();
           flush(() => {
             expect(Polymer.dom(this.root).node.querySelectorAll('li').length).to.equal(3);
-    
+
           });
       });
   });
